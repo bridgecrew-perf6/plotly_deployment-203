430c3b7e90a6a7c7e396fbdb64f488dc3b4cf80b
mcbride249 <92111396+mcbride249@users.noreply.github.com>
2022-01-20 18:04:03 -0500

    Add files via upload

--- chart.js
+++ chart.js
@@ -111,13 +111,13 @@ function buildCharts(sample) {
 
     // 1. Create the trace for the bubble chart.
     var bubbleData = [{
-      x: otu_id.slice[0,10].reverse(),
-      y: sample_values.slice[0,10].reverse(),
-      text: [otu_labels.slice[0,10].reverse()],
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
       mode: "markers",
       marker: {
-        size: [sample_values.slice[0,10].reverse()],
-        color: [otu_id],
+        size: sample_values,
+        color: otu_ids,
         colorscale: "Jet"
       }
     }];
@@ -133,9 +133,36 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
 
 
+    //Deliverable 3 - Gauge Chart
 
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [{
+      domain: { x: [0, 1], y: [0, 1] },
+      value: washFreq,
+      title: { text: "Belly Button Washing Frequency" },
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+      axis: { range: [null, 10], tickwidth: 1, tickcolor: "black" },
+      steps: [
+      { range: [0, 2], color: "violet" },
+      { range: [2, 4], color: "indigo" },
+      { range: [4, 6], color: "blue" },
+      { range: [6, 8], color: "green" },
+      { range: [8, 10], color: "yellow" }
+     ]}
+    }];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 500,
+      height: 400,
+      margin: { t: 25, r: 25, l: 25, b: 25 },
+      font: { color: "black", family: "Arial" }
+    };
 
-
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
 
     
   });
